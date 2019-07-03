23ea7dc00b491293d9da7b9ace2307113c970c8c
younus-md <smdyns97@gmail.com>
2019-07-03 16:07:17 +0530

    adding map

--- src/collections/Maps.java
+++ src/collections/Maps.java
@@ -5,34 +5,66 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
-public class Maps {
+/* Map : is not a child interface of collection.Collection is all about grouping of individual objects but map is collection of key value pairs
+key & value both are objects each key:value pair is an Entry so map is collection of Entry objects based on hash table. No duplicate keys but values can be duplicated.
+Object put(Object add) can have one null key & is non-synchronized
+implements serializable & clonable
+search operation
 
-    void doHashMap(){
-        Map map=new HashMap();
+*/
+public class Maps {
+    /*HashMap has 4 constructors simiar to Hashset
+      HashMap hm= new HashMap() // 16 capacity & 0.75 fill ratio
+      HashMap hm =new HashMap(int initialCapacity)
+      HashMap hm =new HashMap(int initial Capacity, int fillRatio)
+      HashMap hm =new HashMap(Map m)
+    *
+    *
+    * */
+
+    void operateHashMap() {
+        Map<String, Integer> map = new HashMap<>();
+        System.out.println( "hii" + map.put( "name", 1 ) ); // nulll
+        // put method calculates hashcode of key &
+        map.put( "add", 2 );
+        System.out.println( map.put( "name", 3 ) );
+        map.keySet(); // Set is return type gives all the keys
+        map.values();//return type is collection
+        map.entrySet();//Set<Entry<K,v>>
+       /* On Entry we can perform below methods Entry Interface
+        Object getKey(), Object getValue() Object setValue()*/
+        map.put( null, 9 );
+        System.out.println( map );
+
+
+    }
+
+    void doHashMap() {
+        Map map = new HashMap();
         //Adding elements to map
-        map.put(1,"Amit");
-        map.put(5,"Rahul");
-        map.put(2,"Jai");
-        map.put(6,"Amit");
+        map.put( 1, "Amit" );
+        map.put( 5, "Rahul" );
+        map.put( 2, "Jai" );
+        map.put( 6, "Amit" );
         //Traversing Map
-        Set set=map.entrySet();//Converting to Set so that we can traverse
-        Iterator itr=set.iterator();
-        while(itr.hasNext()){
+        Set set = map.entrySet();//Converting to Set so that we can traverse
+        Iterator itr = set.iterator();
+        while (itr.hasNext()) {
             //Converting to Map.Entry so that we can get key and value separately
-            Map.Entry entry=(Map.Entry)itr.next();
-            System.out.println(entry.getKey()+" "+entry.getValue());
+            Map.Entry entry = (Map.Entry) itr.next();
+            System.out.println( entry.getKey() + " " + entry.getValue() );
         }
 
 
-        HashMap<Integer,String> hm=new HashMap<Integer,String>();
-        System.out.println("Initial list of elements: "+hm);
-        hm.put(100,"Amit");
-        hm.put(101,"Vijay");
-        hm.put(102,"Rahul");
+        HashMap<Integer, String> hm = new HashMap<Integer, String>();
+        System.out.println( "Initial list of elements: " + hm );
+        hm.put( 100, "Amit" );
+        hm.put( 101, "Vijay" );
+        hm.put( 102, "Rahul" );
 
-        System.out.println("After invoking put() method ");
-        for(Map.Entry m:hm.entrySet()){
-            System.out.println(m.getKey()+" "+m.getValue());
+        System.out.println( "After invoking put() method " );
+        for (Map.Entry m : hm.entrySet()) {
+            System.out.println( m.getKey() + " " + m.getValue() );
         }
 
     }
