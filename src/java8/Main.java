11da9ce3b3ade72beeb50603012d806a0b7a81e0
younus-md <smdyns97@gmail.com>
2019-07-09 20:04:58 +0530

    adding predicate function & consumer features

--- src/java8/Main.java
+++ src/java8/Main.java
@@ -2,6 +2,10 @@ package java8;
 
 import com.sun.org.apache.bcel.internal.generic.LMUL;
 
+import java.util.ArrayList;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class Main implements DefaultMethods {
 
     @Override
@@ -23,6 +27,51 @@ public class Main implements DefaultMethods {
 
         Test test = new Test();
         test.goToGym();
+        /* Predicate : used for conditional checks replacement of if else statements write once use many times
+        use cases: null checks string length .. returns true or false*/
+
+        Predicate<Integer> predicate = num -> (num % 2) == 0;
+        System.out.println( predicate.test( 14 ) );
+        System.out.println( predicate.test( 15 ) );
+        /* Lets print all elements whose length > 4*/
+        String[] str = {"syed", "mohammed", "javeed"};
+        Predicate<String> predicate1 = str1 -> str1.length() > 4;
+
+        for (String s : str) {
+            if (predicate1.test( s )) {
+
+                System.out.println( "elements" + s );
+            }
+        }
+        /*Applying Predicate on our own Custom class*/
+        ArrayList<SoftwareEngineer> softwareEngineers = new ArrayList<SoftwareEngineer>();
+        softwareEngineers.add( new SoftwareEngineer( 45000, "Iliyas" ) );
+        softwareEngineers.add( new SoftwareEngineer( 4000, "Iliyas" ) );
+        softwareEngineers.add( new SoftwareEngineer( 5000, "Vamsi" ) );
+        softwareEngineers.add( new SoftwareEngineer( 45100, "Javeed" ) );
+        softwareEngineers.add( new SoftwareEngineer( 34440, "Shasha" ) );
+        Predicate<SoftwareEngineer> softwareEngineerPredicate = sfe -> sfe.salary > 45000;
+        for (SoftwareEngineer al : softwareEngineers) {
+            if (softwareEngineerPredicate.test( al )) {
+                System.out.println( al.name
+                );
+
+            }
+            /*Predicate Joining : we can combine two predicates by and  or negate */
+            int x[] = {40, 70, 45, 6, 7, 1000};
+            Predicate<Integer> p1 = num -> (num % 10) == 0;
+            Predicate<Integer> p2 = num -> num > 40;
+            for (int x1 : x) {
+                if (p1.and( p2 ).test( x1 )) {
+                    System.out.println( x1 );
+                }
+            }
+            /*Function Interface*/
+            Function<Integer, Integer> f = i -> i * i;
+            System.out.println( f.apply( 7 ) );
+
+
+        }
 
     }
 }
